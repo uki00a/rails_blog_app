2b862fec3e4970c44fcce9da216b5155b62c06d2
uki00a <uki00a@gmail.com>
2020-12-14 17:04:58 +0900

    Fixed tests

--- client/test/utils.ts
+++ client/test/utils.ts
@@ -4,6 +4,10 @@ import Vuetify from "vuetify"
 
 Vue.use(Vuetify)
 
+const NuxtLinkStub = Vue.extend({
+  template: "<a><slot /></a>"
+})
+
 // Adopted from https://github.com/testing-library/vue-testing-library/blob/master/src/__tests__/vuetify.js
 export const renderWithVuetify: typeof render = (
   component,
@@ -14,6 +18,10 @@ export const renderWithVuetify: typeof render = (
   return render(component, {
     container: root,
     vuetify: new Vuetify(),
+    stubs: {
+      // @see https://github.com/nuxt/nuxt.js/issues/4115
+      "nuxt-link": NuxtLinkStub,
+    },
     ...options,
   })
 }
