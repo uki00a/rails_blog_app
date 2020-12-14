242ecab1431dc26ba79d73e424994fc53d3cb68b
Yuki Tanaka <uki00a@gmail.com>
2020-12-14 19:03:40 +0900

    feat: Add login form (#4)

--- client/types/vue-shims.d.ts
+++ client/types/vue-shims.d.ts
@@ -1,5 +1,14 @@
+// TODO This is not ideal...
+import type { APIClient } from "~/infra/api";
+
 // Adopted from https://github.com/Microsoft/TypeScript-Vue-Starter#single-file-components
 declare module "*.vue" {
   import Vue from "vue";
   export default Vue;
+}
+
+declare module 'vue/types/vue' {
+  interface Vue {
+    $api: APIClient
+  }
 }
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   post 'user_token' => 'user_token#create'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  get "me" => "users#me"
+
   resources :articles
   resources :users do
     member do
